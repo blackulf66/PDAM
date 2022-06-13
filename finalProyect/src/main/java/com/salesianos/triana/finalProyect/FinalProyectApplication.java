e279e650bb37a0e2df170dd9f87c49fc5f3ba37f
Javier Navarro Vega <jnavarro@DES01372.ayesa.es>
2022-06-13 13:17:58 +0200

    añado propiedad para la documentacion

--- finalProyect/src/main/java/com/salesianos/triana/finalProyect/FinalProyectApplication.java
+++ finalProyect/src/main/java/com/salesianos/triana/finalProyect/FinalProyectApplication.java
@@ -2,6 +2,10 @@ package com.salesianos.triana.finalProyect;
 
 import com.salesianos.triana.finalProyect.config.StorageProperties;
 import com.salesianos.triana.finalProyect.service.StorageService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +15,15 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 
+
+@OpenAPIDefinition(info =
+@Info(description = "Weekly bugle API",
+		version = "1.0",
+		contact = @Contact(email = "javiernavarro66@gmail.com", name = "The BlackUlf"),
+		license = @License(name = ":O"),
+		title = "API Weekly Bugle"
+)
+)
 public class FinalProyectApplication {
 
 	public static void main(String[] args) {
